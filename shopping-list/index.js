4d4b247bde8a6a4d7eaaebd57c552fb971895f70
lawstonwalker <54003450+lawstonwalker@users.noreply.github.com>
2020-02-05 16:28:22 -0500

    Add files via upload

---
+++ shopping-list/index.js
@@ -0,0 +1,33 @@
+$(function(){
+        $('#js-shopping-list-form').submit(function(e){
+
+        e.preventDefault();
+
+        let text = $('#shopping-list-entry').val();    
+
+        $('.shopping-list').append(
+            `<li>
+                <span class = "shopping-item">${text}</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle">
+                        <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`
+        )
+
+        $('#shopping-list-entry').val('');
+        
+    });
+
+
+    $('.shopping-list').on('click', '.shopping-item-delete', function(e){
+        $(this).closest('li').remove()});
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(e){
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked')});
+
+})
